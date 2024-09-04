d1fc6867db6c3706de53121d0119286c5c39e5e1
Hugo Moruno <hugomoruno2003@gmail.com>
2024-09-04 13:23:44 +0200

    Creada y organizada la estructura de los css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import './css/App.css';
+import './color.css';
 
 function App() {
   return (
     <div className="App" style={{backgroundColor:'#476382', color:'#ffffff', textAlign:'center'}}>
       <div className="container py-2" >
-        <h1>WIP PAGE</h1>
-        <h1>POKEMON SHARDS</h1>
+        
       </div>
     </div>
   );
