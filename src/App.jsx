c7b23513e142b1d39eb1cdb44cd87824c37ad463
Hugo Moruno <hugomoruno2003@gmail.com>
2024-09-04 10:24:57 +0200

    Página de prueba WIP

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div style={{backgroundColor:'#476382', height:'100px', color:'#ffffff', textAlign:'center'}}>
+        <h1>WIP PAGE</h1>
+        <h1>POKEMON SHARDS</h1>
+      </div>
     </div>
   );
 }
