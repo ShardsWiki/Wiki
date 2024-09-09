1e98f265a47fb6cceb6cb7785faf4c3c8d27f2da
Hugo Moruno <hugomoruno2003@gmail.com>
2024-09-09 23:02:56 +0200

    Ajustado de las fotos (Manual)

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -3,33 +3,33 @@ import '../css/Inicio.css';
 
 export default function Inicio() {
   return (
-    <div className="inicio container">
+    <div className="inicio container"> 
         <div id="photoCarousel" className="carousel slide" data-bs-ride="carousel">
-                <ol className="carousel-indicators">
-                    <li data-bs-target="#photoCarousel" data-bs-slide-to="0" className='list-unstyled active'></li>
-                    <li data-bs-target="#photoCarousel" data-bs-slide-to="1" className='list-unstyled'></li>
-                    <li data-bs-target="#photoCarousel" data-bs-slide-to="2" className='list-unstyled'></li>
-                </ol>
-                <div className="carousel-inner rounded fotos">
-                    <div className="carousel-item active">
-                        <img src="img/foto1.png" className="d-block w-100" alt="1"/>
-                    </div>
-                    <div className="carousel-item">
-                        <img src="img/foto2.png" className="d-block w-100" alt="2"/>
-                    </div>
-                    <div className="carousel-item">
-                        <img src="img/foto3.jpeg" className="d-block w-100" alt="3"/>
-                    </div>
+            <ol className="carousel-indicators">
+                <li data-bs-target="#photoCarousel" data-bs-slide-to="0" className='list-unstyled active'></li>
+                <li data-bs-target="#photoCarousel" data-bs-slide-to="1" className='list-unstyled'></li>
+                <li data-bs-target="#photoCarousel" data-bs-slide-to="2" className='list-unstyled'></li>
+            </ol>
+            <div className="carousel-inner rounded fotos">
+                <div className="carousel-item active">
+                    <img src="img/foto1.png" className="d-block w-100" alt="1"/>
                 </div>
-                <a className="carousel-control-prev" href="#photoCarousel" role="button" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </a>
-                <a className="carousel-control-next" href="#photoCarousel" role="button" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </a>
+                <div className="carousel-item">
+                    <img src="img/foto2.png" className="d-block w-100" alt="2"/>
+                </div>
+                <div className="carousel-item">
+                    <img src="img/foto3.png" className="d-block w-100" alt="3"/>
+                </div>
             </div>
+            <a className="carousel-control-prev" href="#photoCarousel" role="button" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#photoCarousel" role="button" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </a>
+        </div>
     </div>
   )
 }
